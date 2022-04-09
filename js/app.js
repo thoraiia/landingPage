3effb6b6ffb787718a567c7b0132e80b36680cf8
thoraiia <thoraiia.khaled@gmail.com>
2022-04-09 03:57:37 +0200

    Completed

--- js/app.js
+++ js/app.js
@@ -22,7 +22,9 @@
  * Define Global Variables
  * 
 */
-
+let sections = document.body.getElementsByTagName("section");
+let fragmentListItems = document.createDocumentFragment();
+let navBar = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
@@ -39,12 +41,47 @@
 */
 
 // build the nav
-
+for(let i = 0 ; i<sections.length ; i++)
+{
+    let item = document.createElement("li");
+    let id = sections[i].id;
+    let txt = sections[i].getAttribute("data-nav");
+    item.innerHTML = `<a href="#${id}">${txt}</a>`
+    item.classList.add("menu__link")
+    fragmentListItems.appendChild(item);
+}
+navBar.appendChild(fragmentListItems);
 
 // Add class 'active' to section when near top of viewport
 
+let activeClassId = "#section1";
+
+document.addEventListener('scroll',function(){
+
+    for(let i = 0 ; i<sections.length; i++){
+        let rectangle = sections[i].getBoundingClientRect();
+        if(rectangle.top > 0 && rectangle.bottom>0)
+        {
+            // console.log(`current active section is ${sections[i].id}`);
+            // console.log(rectangle.top);
+
+            document.querySelector(activeClassId).classList.remove('your-active-class');
+            activeClassId = `#${sections[i].id}`;
+            sections[i].classList.add('your-active-class');
+
+            break;
+        }
+    }
+});
 
 // Scroll to anchor ID using scrollTO event
+navBar.addEventListener('click', function moveSmoothely(event)
+                        {
+                            event.preventDefault();
+                            let myHref = event.target.getAttribute('href');
+                            document.querySelector(myHref).scrollIntoView({behavior: 'smooth'});
+                        });
+
 
 
 /**
@@ -52,11 +89,5 @@
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
